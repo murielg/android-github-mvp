23e051f1349da2174381bad9710068affeb428c0
Muriel Gonzalez <murielgonzalez@gmail.com>
2018-02-19 23:00:06 -0600

    utils

---
+++ app/src/main/java/com/murielgonzalez/androidmvp/utils/AppConstants.java
@@ -0,0 +1,11 @@
+package com.murielgonzalez.androidmvp.utils;
+
+/**
+ * Created by muriel_gonzalez on 2/19/18.
+ */
+
+public final class AppConstants {
+
+  public static final String PREFS_NAME = "shared_prefs";
+
+}
