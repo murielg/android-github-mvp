67b42a8295fd88d7435ed22049a92e3d651e32e7
Muriel Gonzalez <murielgonzalez@gmail.com>
2018-02-19 22:50:26 -0600

    added kotlin support

--- app/src/main/java/com/murielgonzalez/androidmvp/data/models/Article.kt
+++ app/src/main/java/com/murielgonzalez/androidmvp/data/models/Article.kt
@@ -1,5 +1,28 @@
 package com.murielgonzalez.androidmvp.data.models
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by muriel_gonzalez on 2/19/18.
  */
+
+data class Result(
+        @SerializedName("TotalPages") val totalPages: Int,
+        @SerializedName("CurrentPage") val currentPage: Int,
+        @SerializedName("Results") val results: List<Article>
+)
+
+data class Article (
+        @SerializedName("Content") val content: String,
+        @SerializedName("Title") val title: String,
+        @SerializedName("Author") val author: Author,
+        @SerializedName("Topics") val topics: List<Topic>
+)
+
+data class Author (
+        @SerializedName("Name") val name: String
+)
+
+data class Topic (
+        @SerializedName("Name") val name: String
+)
