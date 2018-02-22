ccf0f2bf2dba118f0bb2d63678d692ccfe4865db
Muriel Gonzalez <murielgonzalez@gmail.com>
2018-02-22 10:59:01 -0600

    di

--- app/src/main/java/com/murielgonzalez/androidmvp/di/components/ActivityComponent.java
+++ app/src/main/java/com/murielgonzalez/androidmvp/di/components/ActivityComponent.java
@@ -1,8 +1,16 @@
 package com.murielgonzalez.androidmvp.di.components;
 
+import com.murielgonzalez.androidmvp.di.PerActivity;
+import com.murielgonzalez.androidmvp.di.modules.ActivityModule;
+import com.murielgonzalez.androidmvp.ui.main.MainActivity;
+
+import dagger.Component;
+
 /**
  * Created by muriel_gonzalez on 2/21/18.
  */
-
+@PerActivity
+@Component(dependencies = ApplicationComponent.class, modules = ActivityModule.class)
 public interface ActivityComponent {
+    void inject(MainActivity activity);
 }
