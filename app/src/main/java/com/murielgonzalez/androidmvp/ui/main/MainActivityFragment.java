f7e87018d975c703be8b64a5989b597eae709d25
Muriel Gonzalez <murielgonzalez@gmail.com>
2018-02-26 17:02:19 -0600

    fragment + presenter

--- app/src/main/java/com/murielgonzalez/androidmvp/ui/main/MainActivityFragment.java
+++ app/src/main/java/com/murielgonzalez/androidmvp/ui/main/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.murielgonzalez.androidmvp.R;
 
 import javax.inject.Inject;
 
@@ -14,18 +19,35 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * Created by muriel_gonzalez on 2/20/18.
  */
 
-public class MainActivityFragment extends Fragment  {
+public class MainActivityFragment extends Fragment implements MainActivityContract.View  {
+
+  @Inject
+  MainActivityContract.Presenter mPresenter;
 
   public final static String TAG = MainActivityFragment.class.getSimpleName();
-
-  private MainActivityContract.Presenter mPresenter;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    Log.d(TAG, "onCreate");
+    Log.d(TAG, "onCreate Fragment");
   }
 
+  @Override
+  public void setLoadingIndicator(boolean active) {
 
+  }
 
+  @Override
+  public void setPresenter(MainActivityContract.Presenter presenter) {
+    mPresenter= checkNotNull(presenter);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+//    mRecyclerView.setAdapter(null);
+//    ButterKnife.unbind(this);
+//    this.mFollowerListListener = null;
+    mPresenter.unsubscribe();
+  }
 }
